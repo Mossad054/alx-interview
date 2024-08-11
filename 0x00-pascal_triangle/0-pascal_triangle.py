f25593d1a325e1420a4f79f5f3d7975ad72ef60b
mossad054 <michaelndungu054@gmail.com>
2024-08-11 16:09:20 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,24 @@
 """
 pascals traingle
 """
-
 def pascal_triangle(n):
     if n <= 0:
         return []
     
     triangle = [[1]]
-    
+   """
+   funtion to check row numbers
+   """
+
     for i in range(1, n):
         prev_row = triangle[-1]
         new_row = [1]
         
         for j in range(1, i):
             new_row.append(prev_row[j-1] + prev_row[j])
-        
+        """
+        defining new row
+        """
         new_row.append(1)
         triangle.append(new_row)
     
